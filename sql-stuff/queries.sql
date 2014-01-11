979df6672c1b0c5503bcb0445b2a8d9a4847824d
TheDOmagazine <psinco@osteopathic.org>
2014-01-10 18:11:13 -0600

    rebuilds tables

---
+++ sql-stuff/queries.sql
@@ -0,0 +1,57 @@
+CREATE TABLE meta (
+  edition ENUM('Peer Reviewer', 'Editorial Board', 'Editorial Advisory Board') NOT NULL,
+  quarter ENUM('Winter', 'Spring', 'Summer', 'Fall') NOT NULL,
+  `year` YEAR(4) NOT NULL,
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY
+) engine=InnoDB CHARSET=utf8 
+
+create table lead_in (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  conn_id MEDIUMINT NOT NULL,
+  lead_in text,
+  kicker text
+)
+
+create table body (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  conn_id MEDIUMINT NOT NULL,
+  headline TEXT,
+  main TEXT
+) engine InnoDB CHARSET=utf8 
+
+create table poll (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  question TEXT NOT NULL,
+  link TEXT NOT NULL
+) engine=InnoDB CHARSET=utf8 
+
+create table resources (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `resource` TEXT,
+  link TEXT,
+  conn_id MEDIUMINT NOT NULL ,
+  foreign key (conn_id) references meta(id)
+) engine=InnoDB CHARSET=utf8 
+
+create table peer (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(256) NOT NULL,
+  occupation VARCHAR(256) NOT NULL,
+  school VARCHAR(256) NOT NULL,
+  involved TEXT,
+  reviewing TEXT
+) engine=InnoDB CHARSET=utf8 
+
+create table issue (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  conn_id MEDIUMINT NOT NULL,
+  body_id MEDIUMINT NOT NULL,
+  resources_id MEDIUMINT NOT NULL,
+  poll_id MEDIUMINT NOT NULL,
+  peer_id MEDIUMINT NOT NULL,
+  foreign key (conn_id) references meta(id),
+  foreign key (body_id) references body(id),
+  foreign key (resources_id) references resources(id),
+  foreign key (peer_id) references peer(id),
+  foreign key (poll_id) references poll(id)
+)
