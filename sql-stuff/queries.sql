26de3d6deb9e8ce9e165fcf0e2e79f9ff3e6d2a4
TheDOmagazine <psinco@osteopathic.org>
2014-01-10 19:30:20 -0600

    mods queries.sql

--- sql-stuff/queries.sql
+++ sql-stuff/queries.sql
@@ -1,3 +1,22 @@
+create table issue (
+  id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  edition ENUM('Peer Reviewer', 'Editorial Board', 'Editorial Advisory Board') NOT NULL,
+  quarter ENUM('Winter', 'Spring', 'Summer', 'Fall') NOT NULL,
+  `year` YEAR(4) NOT NULL,
+  headline TEXT NOT NULL,
+  main TEXT NOT NULL,
+  lead_in text NOT NULL,
+  kicker text,
+  question TEXT NOT NULL,
+  link TEXT NOT NULL,
+  name VARCHAR(256) NOT NULL,
+  occupation VARCHAR(256) NOT NULL,
+  school VARCHAR(256) NOT NULL,
+  involved TEXT,
+  reviewing TEXT
+) engine=InnoDB CHARSET=utf8 
+
+--------------------------------------------------------------------------------
 CREATE TABLE meta (
   id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   edition ENUM('Peer Reviewer', 'Editorial Board', 'Editorial Advisory Board') NOT NULL,
@@ -31,14 +50,12 @@ create table peer (
 
 create table issue (
   id MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  conn_id MEDIUMINT NOT NULL,
+  conn_id MEDIUMINT,
   body_id MEDIUMINT,
-  resources_id MEDIUMINT,
   poll_id MEDIUMINT,
   peer_id MEDIUMINT,
   foreign key (conn_id) references meta(id),
   foreign key (body_id) references body(id),
-  foreign key (resources_id) references resources(id),
   foreign key (peer_id) references peer(id),
   foreign key (poll_id) references poll(id)
 )
