dc185697f7cd20ce7cbec5d2d7366332f392982d
TheDOmagazine <psinco@osteopathic.org>
2014-01-22 16:58:14 -0600

    fiddles with position of admin preview, code controls

--- js/admin_control_panel_preview.js
+++ js/admin_control_panel_preview.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
     //console.log($(this).prop('id'));
     //$(this).css('background-color', 'darkorange');
     $(this).animate({
-      left: '890px'
+      left: '895px'
     }, 'fast');
   }, function () {
     //$(this).css('background-color', 'ghostwhite');
     $(this).animate({
-      left: '828px'
+      left: '833px'
     }, 'fast');
   }
   );
