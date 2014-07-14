c3d2a4652090c7f57ad9537f2f86042e9b728f1f
pjsinco <psinco@fas.harvard.edu>
2014-07-14 11:46:03 -0500

    sets up twitter_counts

---
+++ controllers/c_twitter_counts.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once(APP_PATH . '/config/constants.php');
+
+class twitter_counts_controller extends base_controller {
+	
+  public function __construct() {
+  	parent::__construct();
+  } 
+  	
+  public function index() {
+    $this->template->content = View::instance('v_twitter_counts_index');
+
+    $client_files_head = Array(
+      '/css/twitter_counts.css'
+    );
+
+    $this->template->client_files_head = 
+      Utils::load_client_files($client_files_head);
+
+    $client_files_body = Array(
+      '/js/twitter_counts_index.js'
+    );
+
+    $this->template->client_files_body = 
+      Utils::load_client_files($client_files_body);
+
+    // render view
+    echo $this->template;
+  }
+
+
+} # End of class
